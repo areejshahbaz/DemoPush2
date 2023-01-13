dace0858b7215c6c56763be7129ef66e0201a66d
areejshahbaz <areejshahbaz@localhost>
2023-01-12 19:53:44 -0500

    second message of Git

--- src/com/nooeteck/git/GitHub.java
+++ src/com/nooeteck/git/GitHub.java
@@ -5,4 +5,9 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("GitHub is fun");
 	}
+	
+	public void multiplication(int a, int b) {
+		System.out.println(a * b);
+		
+	}
 }
